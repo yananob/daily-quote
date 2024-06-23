d04b4f8a48a7d1fc3142080094c6d2c0244d13a7
yananob <yananob@gmail.com>
2024-06-23 13:39:20 +0900

    renamed dir

--- src/database/seeders/QuoteTestSeeder.php
+++ src/database/seeders/QuoteTestSeeder.php
@@ -4,5 +4,5 @@ namespace Database\Seeders;
 
 class QuoteTestSeeder extends QuoteSeeder
 {
-    protected const CSV_FILE = '2024-06-22-dailyquotes_test.csv';
+    protected const CSV_FILE = '2024-06-22_daily-quotes_quotes_quotes.csv';
 }
