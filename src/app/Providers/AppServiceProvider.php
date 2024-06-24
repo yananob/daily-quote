f8a46f56949537c166237b5fda13685c69d6e0cb
yananob <yananob@gmail.com>
2024-06-24 14:05:35 +0900

    added db initialization process

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -26,19 +26,27 @@ class AppServiceProvider extends ServiceProvider
     {
         \Illuminate\Pagination\Paginator::useBootstrap();
 
-        // $this->__retrieveDatabase();
+        $this->__retrieveDatabase();
     }
 
-    // private function __retrieveDatabase(): void
-    // {
-    //     // memo: まだLaravelが立ち上がってないためか、envでは環境変数が取れない
-    //     // （コンテナじゃない場合もエラーが出ないが、この場合はgcloud configの情報を取ってそう）
-    //     $client = new StorageClient([
-    //         'keyFile' => json_decode(file_get_contents(config_path('gcp_serviceaccount.json')), true)
-    //     ]);
-    //     $client = new StorageClient();
-    //     $bucket = $client->bucket(self::BUCKET_NAME);
-    //     $object = $bucket->object(self::FILENAME);
-    //     $object->downloadToFile(database_path(self::FILENAME));
-    // }
+    private function __retrieveDatabase(): void
+    {
+        print("Retrieving database\n");
+        $bucketName = env('MYAPP_CLOUD_BUCKET_NAME');
+        $dbFilename = env('MYAPP_SQLITE_FILENAME');
+        if (empty($bucketName) || empty($dbFilename)) {
+            throw new \Exception('Please specify MYAPP_CLOUD_BUCKET_NAME and MYAPP_SQLITE_FILENAME.');
+        }
+
+        // memo: まだLaravelが立ち上がってないためか、envでは環境変数が取れない
+        // （コンテナじゃない場合もエラーが出ないが、この場合はgcloud configの情報を取ってそう）
+        $client = new StorageClient([
+            'keyFile' => json_decode(file_get_contents(config_path('gcp_serviceaccount.json')), true)
+        ]);
+        $client = new StorageClient();
+        $bucket = $client->bucket($bucketName);
+        $object = $bucket->object($dbFilename);
+        $object->downloadToFile(database_path($dbFilename));
+        print("Retrieved database.\n");
+    }
 }
