931edb78ea3abd0c052ae750c070692c0e953b8d
yananob <yananob@gmail.com>
2025-12-14 18:59:46 +0900

    removed bootstrap_local

--- tests/listen_local_http.sh
+++ tests/listen_local_http.sh
@@ -6,7 +6,6 @@ source ./_cf-common/test/export_secrets.sh ${SECRETS[*]}
 
 # Launch function
 export FUNCTION_TARGET=main_http
-# php -S localhost:8080 tests/bootstrap_local.php
 APP_ENV=local php -S localhost:8080 vendor/bin/router.php
 
 source ./_cf-common/test/unset_secrets.sh ${SECRETS[*]}
